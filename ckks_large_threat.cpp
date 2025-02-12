c64989dfc1fc332a9b77ab24da52287e6a2186cb
plrkiran123 <plrkiran54@gmail.com>
2025-02-12 18:29:45 +0100

    Update ckks_large_threat.cpp
    Adding the function to choose BGV and CKKS

--- ckks_large_threat.cpp
+++ ckks_large_threat.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <random>
 #include "openfhe.h"
 
 using namespace std;
@@ -9,9 +10,12 @@ using namespace lbcrypto;
 vector<double> GenerateLargeThreatDataset(size_t data_size) {
     vector<double> dataset;
     dataset.reserve(data_size);
-    srand(time(nullptr)); 
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(0.0, 1.0); 
+
     for (size_t i = 0; i < data_size; i++) {
-        dataset.push_back((rand() % 1000) / 1000.0); 
+        dataset.push_back(dis(gen));
     }
     return dataset;
 }
@@ -22,12 +26,27 @@ void PrintExecutionTime(const string& operation, chrono::high_resolution_clock::
 }
 
 int main() {
-    CCParams<CryptoContextCKKS> parameters;
-    parameters.SetMultiplicativeDepth(6); 
-    parameters.SetScalingModSize(50);
-    parameters.SetBatchSize(8192); 
+    string scheme_choice;
+    cout << "Choose encryption scheme (CKKS/BGV): ";
+    cin >> scheme_choice;
 
-    CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
+    CryptoContext<DCRTPoly> cc;
+    if (scheme_choice == "CKKS" || scheme_choice == "ckks") {
+        CCParams<CryptoContextCKKS> parameters;
+        parameters.SetMultiplicativeDepth(6);
+        parameters.SetScalingModSize(50);
+        parameters.SetBatchSize(16384); 
+        cc = GenCryptoContext(parameters);
+    } else if (scheme_choice == "BGV" || scheme_choice == "bgv") {
+        CCParams<CryptoContextBGV> parameters;
+        parameters.SetMultiplicativeDepth(6);
+        parameters.SetPlaintextModulus(65537);
+        cc = GenCryptoContext(parameters);
+    } else {
+        cout << "Invalid scheme selected. Please choose CKKS or BGV." << endl;
+        return 1;
+    }
+
     cc->Enable(PKE);
     cc->Enable(KEYSWITCH);
     cc->Enable(LEVELEDSHE);
@@ -36,37 +55,37 @@ int main() {
     cc->EvalMultKeyGen(keyPair.secretKey);
     cc->EvalRotateKeyGen(keyPair.secretKey, {1, -1});
 
-    size_t dataset_size = 1'000'000; // 1 million records
+    size_t dataset_size = 10'000'000; // 10 million records
     cout << "Generating Large Threat Dataset: " << dataset_size << " records..." << endl;
     vector<double> threat_scores = GenerateLargeThreatDataset(dataset_size);
 
     auto start_enc = chrono::high_resolution_clock::now();
-    auto plaintext = cc->MakeCKKSPackedPlaintext(threat_scores);
+    auto plaintext = cc->MakePackedPlaintext(threat_scores);
     auto ciphertext = cc->Encrypt(keyPair.publicKey, plaintext);
     auto end_enc = chrono::high_resolution_clock::now();
     PrintExecutionTime("Encryption", start_enc, end_enc);
 
     auto start_comp = chrono::high_resolution_clock::now();
-    
+
     auto sum_cipher = ciphertext;
     for (size_t i = 1; i < dataset_size; i++) {
         auto rotated_cipher = cc->EvalRotate(ciphertext, i);
         sum_cipher = cc->EvalAdd(sum_cipher, rotated_cipher);
     }
 
-    auto divisor = cc->MakeCKKSPackedPlaintext({(double)dataset_size});
+    auto divisor = cc->MakePackedPlaintext({(double)dataset_size});
     auto mean_cipher = cc->EvalMult(sum_cipher, divisor);
-    
+
     auto squared_scores = cc->EvalMult(ciphertext, ciphertext);
     auto squared_sum = squared_scores;
     for (size_t i = 1; i < dataset_size; i++) {
         auto rotated_cipher = cc->EvalRotate(squared_scores, i);
         squared_sum = cc->EvalAdd(squared_sum, rotated_cipher);
     }
-    
+
     auto mean_squared = cc->EvalMult(mean_cipher, mean_cipher);
     auto variance_cipher = cc->EvalSub(squared_sum, mean_squared);
-    auto stddev_cipher = cc->EvalMult(variance_cipher, cc->MakeCKKSPackedPlaintext({0.5}));
+    auto stddev_cipher = cc->EvalMult(variance_cipher, cc->MakePackedPlaintext({0.5})); 
 
     auto end_comp = chrono::high_resolution_clock::now();
     PrintExecutionTime("Homomorphic Computation", start_comp, end_comp);
@@ -86,3 +105,4 @@ int main() {
 
     return 0;
 }
+
